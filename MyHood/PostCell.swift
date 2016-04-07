99a8600439205d7d584200e8435eb6f59ba484f2
Ryan Collins <ryan@ryanmcollins.com>
2016-04-07 18:41:08 -0400

    Custom Table View Cell Layout Complete

---
+++ MyHood/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  MyHood
+//
+//  Created by Ryan Collins on 4/7/16.
+//  Copyright © 2016 Ryan Collins. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+}
